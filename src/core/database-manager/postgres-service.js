2121a7ffb5c59f6b8d332fb8d9be124153662ed9
Sahar <sanaie.sahar@gmail.com>
2024-06-07 22:33:58 +0330

    use secrets file to postgres

--- src/core/database-manager/postgres-service.js
+++ src/core/database-manager/postgres-service.js
@@ -1,31 +1,22 @@
-import pkg from 'pg'; // Import the pg library as a default package
-import dotenv from 'dotenv'; // Importing the dotenv library to manage environment variables
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+import { secrets } from '../secrets/index.js';
 
-const { Pool } = pkg; // Extracting Pool from the imported package
+dotenv.config();
 
-dotenv.config(); // Load environment variables from .env file
-
-// Create a new PostgreSQL pool using environment variables with SSL enabled
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: 5432,
-  ssl: {
-    rejectUnauthorized: false // Set to false to allow self-signed certificates
-  }
+    user: secrets.PGUSER,
+    host: secrets.PGHOST,
+    database: secrets.PGDATABASE,
+    password: secrets.PGPASSWORD,
+    port: 5432,
+    ssl: {
+      rejectUnauthorized: false // Set to false to allow self-signed certificates
+    }
 });
 
-// Function to execute a query and return the results
-const executeQuery = async (query, values) => {
-  const client = await pool.connect(); // Get a client from the pool
-  try {
-    const result = await client.query(query, values); // Execute the query
-    return result.rows; // Return the rows of the result
-  } finally {
-    client.release(); // Release the client back to the pool
-  }
+export const query = async (text, params) => {
+    const res = await pool.query(text, params);
+    return res.rows;
 };
-
-export { executeQuery }; // Export the executeQuery function
